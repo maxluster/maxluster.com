1e6509e5622389323cff6e2bb518112f040cf705
Max Luster <maxluster@gmail.com>
2018-10-09 01:15:27 -0700

    styled components demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  margin: 3rem auto;
+  background: black;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Container>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Container>
   </Layout>
 )
 
